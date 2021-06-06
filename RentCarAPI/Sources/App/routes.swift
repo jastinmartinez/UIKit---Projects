a99ff586469051820261531d01823138a9a906e6
Jastin Martinez <36866323+jastinmartinez@users.noreply.github.com>
2021-06-06 17:54:19 -0400

    register controller

--- RentCarAPI/Sources/App/routes.swift
+++ RentCarAPI/Sources/App/routes.swift
@@ -7,5 +7,6 @@ func routes(_ app: Application) throws {
     try app.register(collection: VehicleMarkController())
     try app.register(collection: VehicleModelController())
     try app.register(collection: VehicleTypeController())
+    try app.register(collection: CustomerController())
     try app.register(collection: UserController())
 }
