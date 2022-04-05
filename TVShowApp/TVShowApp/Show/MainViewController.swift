7d7458c303b119342c61e226533204e6d2c5ca56
Jastin Martiez <martinez.jastin@hotmail.com>
2022-04-05 09:32:37 -0400

    separate functionality tab controller

--- TVShowApp/TVShowApp/Show/MainViewController.swift
+++ TVShowApp/TVShowApp/Show/MainViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 import PresentationLayer
 
 class MainViewController: UITabBarController {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setConfiguration()
+        self.setViewConfiguration()
+        self.setViewControlellers()
     }
     
-    fileprivate func setConfiguration() {
+    fileprivate func setViewConfiguration() {
         self.tabBar.isTranslucent = false
         self.tabBar.tintColor = UIColor(named: ColorHelper.red.rawValue)!
         self.view.backgroundColor = UIColor(named: ColorHelper.blue.rawValue)!
+    }
+    
+    fileprivate func setViewControlellers() {
         let showViewModel = ShowDependencyInjection().setShowViewModelDependecy()
+        
         let showViewController = UINavigationController(rootViewController: ShowTableViewController(showViewModel: showViewModel))
         showViewController.navigationBar.backgroundColor = UIColor(named: ColorHelper.blue.rawValue)!
         showViewController.tabBarItem = UITabBarItem(title: "TV Shows", image: UIImage(systemName: "ticket"), selectedImage: UIImage(systemName: "ticket"))
+        
         let configurationViewController =  ConfigurationViewController()
         configurationViewController.tabBarItem = UITabBarItem(title: "Configuration", image: UIImage(systemName: "gear.circle"), selectedImage: UIImage(systemName: "gear.circle"))
+        
         self.setViewControllers([ showViewController,configurationViewController], animated: false)
     }
 }
