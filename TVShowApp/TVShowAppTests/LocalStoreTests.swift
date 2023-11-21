d9a3a2683c1fb7179591119afcafcef577de0812
Jastin Martinez <martinez.jastin@hotmail.com>
2023-11-20 20:06:55 -0400

    test localStore store a simple value

--- TVShowApp/TVShowAppTests/LocalStoreTests.swift
+++ TVShowApp/TVShowAppTests/LocalStoreTests.swift
@@ -8,10 +8,24 @@
 import XCTest
 import TVShowApp
 
-
 final class LocalStoreTests: XCTestCase {
 
-    func test() {
+    func test_localStore_storeAValue() {
+        let sut = LocalStore()
         
+        sut.save(for: "key", with: "value")
+        
+        XCTAssertEqual(sut.dic.count, 1)
     }
 }
+
+final class LocalStore {
+    
+    private(set) var dic = [String: String]()
+    
+    func save(for key: String, with value: String) {
+        dic[key] = value
+    }
+}
+
+
