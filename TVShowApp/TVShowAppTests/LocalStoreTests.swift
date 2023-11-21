1a9d50beb607f8e101043c2b7699fac6f479c940
Jastin Martinez <martinez.jastin@hotmail.com>
2023-11-20 21:06:14 -0400

    add some localStore test to handle biometrics

--- TVShowApp/TVShowAppTests/LocalStoreTests.swift
+++ TVShowApp/TVShowAppTests/LocalStoreTests.swift
@@ -9,22 +9,117 @@ import XCTest
 import TVShowApp
 
 final class LocalStoreTests: XCTestCase {
-
-    func test_localStore_storeAValue() {
-        let sut = LocalStore()
+    
+    func test_localStore_saveAValue() {
+        let (sut, key) = makeSUT()
         
-        sut.save(for: "key", with: "value")
+        sut.save(for: "key", with: true)
         
-        XCTAssertEqual(sut.dic.count, 1)
+        expect(when: sut.get(for: key), expect: .success(true))
+    }
+    
+    func test_localStore_DoNotDuplicateWhenSaving() {
+        let (sut, key) = makeSUT()
+        
+        sut.save(for: key, with: true)
+        sut.save(for: key, with: true)
+        
+        XCTAssertEqual(1, sut.count)
+    }
+    
+    func test_localStore_ReplaceWithLatestValue() {
+        let (sut, key) = makeSUT()
+        
+        sut.save(for: key, with: true)
+        sut.save(for: key, with: false)
+        
+        expect(when: sut.get(for: key), expect: .success(false))
+    }
+    
+    
+    func test_localStore_deliversErrorWhetNotFound() {
+        let (sut, key) = makeSUT()
+        
+        let result = sut.get(for: key)
+        
+        expect(when: result, expect: .failure(NSError(domain: "Key not Found", code: 0)))
+    }
+    
+    private func makeSUT() -> (sut: LocalStoreAdapter, key: String) {
+        let mock = MockStore()
+        let key = "key"
+        let sut = LocalStoreAdapter(localStore: mock)
+        return (sut, key)
+    }
+    
+    private func expect(when complete: Result<Bool, Error>,
+                        expect value: Result<Bool, Error> ,
+                        file: StaticString = #filePath,
+                        line: UInt = #line) {
+        switch(value, complete) {
+        case let (.success(expectedSuccess), .success(actualSuccess)):
+            XCTAssertEqual(expectedSuccess,
+                           actualSuccess,
+                           file: file,
+                           line: line)
+        case let (.failure(expectedFailure), .failure(actualFailure)):
+            XCTAssertEqual(expectedFailure.localizedDescription,
+                           actualFailure.localizedDescription,
+                           file: file,
+                           line: line)
+        default:
+            XCTFail("not match expected")
+        }
     }
 }
 
-final class LocalStore {
+
+protocol LocalStore {
+    func get(for key: String) -> Result<Bool, Error>
+    func save(for key: String, with value: Bool)
+    func count() -> Int
+}
+
+final class MockStore: LocalStore {
     
-    private(set) var dic = [String: String]()
+    private var dic = [String: Bool]()
     
-    func save(for key: String, with value: String) {
+    func get(for key: String) -> Result<Bool, Error> {
+        if let value = dic[key] {
+            return .success(value)
+        } else {
+            return .failure(NSError(domain: "Key not Found", code: 0))
+        }
+    }
+    
+    func save(for key: String, with value: Bool) {
         dic[key] = value
+    }
+    
+    func count() -> Int {
+        return dic.count
+    }
+}
+
+
+final class LocalStoreAdapter {
+    
+    private let localStore: LocalStore
+    
+    var count: Int  {
+        return localStore.count()
+    }
+    
+    init(localStore: LocalStore) {
+        self.localStore = localStore
+    }
+    
+    func save(for key: String, with value: Bool) {
+        localStore.save(for: key, with: value)
+    }
+    
+    func get(for key: String) -> Result<Bool, Error> {
+        return localStore.get(for: key)
     }
 }
 
