ddc123504be1a828227c78b81e5468d19541fb21
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-27 16:02:10 -0400

    initial project structure

---
+++ CatApp/CatApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CatApp
+//
+//  Created by Jastin on 27/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
