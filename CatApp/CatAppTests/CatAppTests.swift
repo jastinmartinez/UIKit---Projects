c0fb0b38f02e2f268b1d46cab964433269879178
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-27 16:59:19 -0400

    make sure `URLSessionHTTPClient` delivers data from request

--- CatApp/CatAppTests/CatAppTests.swift
+++ CatApp/CatAppTests/CatAppTests.swift
@@ -15,16 +15,18 @@ final class URLSessionHTTPClient {
         self.session = session
     }
     
-    func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in
-            
+    func get(from url: URL, completion: @escaping (Data) -> Void = {_ in }) {
+        session.dataTask(with: url) { data, _, _ in
+            if let data = data {
+                completion(data)
+            }
         }.resume()
     }
 }
 
 final class CatAppTests: XCTestCase {
 
-    func test_URLSessionHTTPClient_performRequestFromURL() {
+    func test_getFromURL_performRequestFromURL() {
         URLProtocol.registerClass(MockSession.self)
         let sut = URLSessionHTTPClient()
         let anyURL = URL(string: "https://www.any-url.com")!
@@ -39,12 +41,39 @@ final class CatAppTests: XCTestCase {
         wait(for: [waitForRequest], timeout: 1.0)
         URLProtocol.unregisterClass(MockSession.self)
     }
+    
+    func test_getFromURL_deliversData() {
+        URLProtocol.registerClass(MockSession.self)
+       
+        let sut = URLSessionHTTPClient()
+        let anyURL = URL(string: "https://www.any-url.com")!
+        let waitForRequest = expectation(description: "wait for async call")
+        let anyData = Data()
+        MockSession.stub(data: anyData)
+        
+        sut.get(from: anyURL, completion: { data in
+            XCTAssertEqual(anyData, data)
+            waitForRequest.fulfill()
+        })
+        
+        wait(for: [waitForRequest], timeout: 1.0)
+        URLProtocol.unregisterClass(MockSession.self)
+    }
 }
 
 private class MockSession: URLProtocol {
     
     static var listenToRequest: ((URLRequest) -> Void)? = nil
+    private static var stub: Stub? = nil
     
+    private struct Stub {
+        let data: Data
+    }
+    
+    static func stub(data: Data) {
+        stub = Stub(data: data)
+    }
+
     override class func canInit(with request: URLRequest) -> Bool {
         return true
     }
@@ -56,6 +85,11 @@ private class MockSession: URLProtocol {
     override func startLoading() {
         if let stub = MockSession.listenToRequest {
             stub(request)
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        if let stub = MockSession.stub {
+            client?.urlProtocol(self, didLoad: stub.data)
             client?.urlProtocolDidFinishLoading(self)
         }
     }
