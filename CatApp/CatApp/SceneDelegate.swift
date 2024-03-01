18f5d3c506ced854709ca0250f15ce45eda731d9
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-29 22:13:35 -0400

    working with catCell content

--- CatApp/CatApp/SceneDelegate.swift
+++ CatApp/CatApp/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let url = URL(string: "https://cataas.com/api/cats?tags=cute")!
+        let url = URL(string: "https://cataas.com/api/cats")!
         let client = URLSessionHTTPClient()
         let remoteCatLoader = RemoteCatLoader(url: url, client: client)
         let remoteImageLoader = RemoteImageLoader(client: client)
--- CatApp/CatApp/ui/cell/CatTableViewCell.swift
+++ CatApp/CatApp/ui/cell/CatTableViewCell.swift
@@ -10,13 +10,14 @@ import UIKit
 
 public class CatTableViewCell: UITableViewCell, IdentifiableCell {
     
-    let catImageView: UIImageView = {
+    private let catImageView: UIImageView = {
         let xCatImageView = UIImageView()
         xCatImageView.layer.masksToBounds = true
         xCatImageView.layer.cornerRadius = 20
         xCatImageView.translatesAutoresizingMaskIntoConstraints = false
         return xCatImageView
     }()
+    
     
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -49,23 +50,73 @@ public class CatTableViewCell: UITableViewCell, IdentifiableCell {
     }
     
     func setCat(_ cat: Cat) {
+        let tagStackView = tagStackViewFactory()
+        if let tags = cat.tags {
+            for index in 0..<min(3,tags.count) {
+                setTagView(tags[index], tagStackView: tagStackView)
+            }
+        }
+    }
+    
+    private func setTagView(_ tag: String, tagStackView: UIStackView) {
+        let tagLabel = tagLabelFactory(with: tag)
+        let tagView = tagViewFactory()
+        tagView.addSubview(tagLabel)
+        tagStackView.addArrangedSubview(tagView)
+        NSLayoutConstraint.activate([tagLabel.topAnchor.constraint(equalTo: tagView.layoutMarginsGuide.topAnchor),
+                                     tagLabel.leadingAnchor.constraint(equalTo: tagView.layoutMarginsGuide.leadingAnchor),
+                                     tagLabel.trailingAnchor.constraint(equalTo: tagView.layoutMarginsGuide.trailingAnchor),
+                                     tagLabel.bottomAnchor.constraint(equalTo: tagView.layoutMarginsGuide.bottomAnchor)])
         
+        addSubview(tagStackView)
+        NSLayoutConstraint.activate([tagStackView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+                                     tagStackView.leadingAnchor.constraint(equalTo: catImageView.trailingAnchor, constant: 10)])
+    }
+    
+    private func tagStackViewFactory() -> UIStackView {
+        let tagStackView = UIStackView()
+        tagStackView.translatesAutoresizingMaskIntoConstraints = false
+        tagStackView.axis = .horizontal
+        tagStackView.distribution = .fill
+        tagStackView.alignment = .leading
+        tagStackView.spacing = 5
+        return tagStackView
+    }
+    
+    private func tagLabelFactory(with text: String) -> UILabel {
+        let tagLabel = UILabel()
+        tagLabel.translatesAutoresizingMaskIntoConstraints = false
+        tagLabel.text = text
+        return tagLabel
+    }
+    
+    private func tagViewFactory() -> UIView {
+        let tagView = UIView()
+        tagView.translatesAutoresizingMaskIntoConstraints = false
+        tagView.layer.borderWidth = 1
+        tagView.layer.borderColor = UIColor.systemBlue.cgColor
+        tagView.layer.cornerRadius = 5
+        return tagView
     }
     
     func setCatImage(_ image: DataStatePresenter<Data>) {
-        DispatchQueue.main.async { [catImageView] in
-            switch image {
-            case .loading:
-                catImageView.image = UIImage(named: AssetConstant.catPlaceholder.rawValue)
-            case .success(let data):
-                if let image = UIImage(data: data) {
-                    catImageView.image = image
-                } else {
-                    catImageView.image = UIImage(named: AssetConstant.catNotFound.rawValue)
-                }
-            case .failure:
+        DispatchQueue.main.async { [setImageFor] in
+            setImageFor(image)
+        }
+    }
+    
+    private func setImageFor(_ state: DataStatePresenter<Data>) {
+        switch state {
+        case .loading:
+            catImageView.image = UIImage(named: AssetConstant.catPlaceholder.rawValue)
+        case .success(let data):
+            if let image = UIImage(data: data) {
+                catImageView.image = image
+            } else {
                 catImageView.image = UIImage(named: AssetConstant.catNotFound.rawValue)
             }
+        case .failure:
+            catImageView.image = UIImage(named: AssetConstant.catNotFound.rawValue)
         }
     }
 }
